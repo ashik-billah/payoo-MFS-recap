ca614d064cfd9272fb1884d4ea9c14f2dba5f80c
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-02-27 19:43:16 +0600

    add money event handler

---
+++ js/addMoney.js
@@ -0,0 +1,34 @@
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+   
+
+    const addMoney = document.getElementById('input-add-money').value;
+    const addMoneyNumber = parseFloat(addMoney);
+
+    const pinNumber = document.getElementById('input-pin-number').value;
+
+
+
+
+    console.log(addMoney,pinNumber);
+
+    //wrong way to verify pin
+
+    if(pinNumber === '123')
+    {
+       const  balance = document.getElementById('account-balance').innerText;
+       
+       const balanceNumber = parseFloat(balance);
+       const newBalance = addMoneyNumber + balanceNumber;
+       console.log(newBalance);
+       document.getElementById('account-balance').innerText = newBalance;
+
+
+
+    }
+    else
+    {
+       alert('Failed to add money')
+    }
+    
+})
